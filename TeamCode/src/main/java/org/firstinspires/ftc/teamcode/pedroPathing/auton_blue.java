e7d3937c6cabcde12f6bf0654f6742902873592d
MohammadSyed18 <79668964+MohammadSyed18@users.noreply.github.com>
2025-11-15 19:49:20 -0600

    basic for autonblue
    Refactor auton_blue to extend AutonomousPLUS and update paths.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/auton_blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/auton_blue.java
@@ -1,80 +1,62 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package org.firstinspires.ftc.teamcode.Decode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
-import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathBuilder;
-import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Vector2d;
-import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-@Autonomous(name = "auton_blue")
-public class _auton_blue extends LinearOpMode {
-    @Override
-    public void runOpMode() {
-        // TODO: Initialize your drive system here
-        // Example: Follower follower = new Follower(hardwareMap);
-        
-        waitForStart();
-        
-        // Starting pose from your trajectory
-        Pose2d startPose = new Pose2d(36.61, 135.74, Math.toRadians(90));
-        
-        // Build and execute paths
-        Path path1 = new PathBuilder(startPose)
-            .lineTo(new Vector2d(68, 83))
-            .setTangent(Math.toRadians(180))
-            .build();
-        // follower.followPath(path1);
-        
-        Path path2 = new PathBuilder(path1.getEndPose())
-            .lineTo(new Vector2d(20.53, 83.97))
-            .setTangent(Math.toRadians(180))
-            .build();
-        // follower.followPath(path2);
-        
-        Path path3 = new PathBuilder(path2.getEndPose())
-            .lineTo(new Vector2d(68, 83))
-            .setTangent(Math.toRadians(-180))
-            .build();
-        // follower.followPath(path3);
-        
-        Path path4 = new PathBuilder(path3.getEndPose())
-            .lineTo(new Vector2d(44.79, 60.03))
-            .setTangent(Math.toRadians(-180))
-            .build();
-        // follower.followPath(path4);
-        
-        Path path5 = new PathBuilder(path4.getEndPose())
-            .lineTo(new Vector2d(19.90, 59.40))
-            .setTangent(Math.toRadians(-180))
-            .build();
-        // follower.followPath(path5);
-        
-        Path path6 = new PathBuilder(path5.getEndPose())
-            .lineTo(new Vector2d(68, 83))
-            .setTangent(Math.toRadians(-180))
-            .build();
-        // follower.followPath(path6);
-        
-        Path path7 = new PathBuilder(path6.getEndPose())
-            .lineTo(new Vector2d(43.37, 35.11))
-            .setTangent(Math.toRadians(-180))
-            .build();
-        // follower.followPath(path7);
-        
-        Path path8 = new PathBuilder(path7.getEndPose())
-            .lineTo(new Vector2d(18.40, 35.30))
-            .setTangent(Math.toRadians(-180))
-            .build();
-        // follower.followPath(path8);
-        
-        Path path9 = new PathBuilder(path8.getEndPose())
-            .lineTo(new Vector2d(68, 83))
-            .setTangent(Math.toRadians(-180))
-            .build();
-        // follower.followPath(path9);
+import org.firstinspires.ftc.teamcode.Decode.AutonomousPLUS;
 
-        telemetry.addData("Status", "Autonomous Complete");
-        telemetry.update();
+@Autonomous(group = "Basic", name = "Blue Auton") //TODO name your auto
+public class _auton_blue extends AutonomousPLUS {
+
+    public void runOpMode() { // This function will run when you initialize the program
+
+        super.runOpMode(); // Robot constructor
+
+        waitForStart(); // Waits for start
+
+        //TODO add your step-by step code here using functions from AutonomousPLUS
+        
+        // Starting pose: (36.61, 135.74) heading 90 degrees
+        // Path 1: Move to (68, 83)
+        moveToPosition(68, 83, 180, 0.5);
+        prepareNextAction(1000);
+        
+        // Path 2: Move to (20.53, 83.97)
+        moveToPosition(20.53, 83.97, 180, 0.5);
+        prepareNextAction(1000);
+        
+        // Path 3: Move to (68, 83)
+        moveToPosition(68, 83, -180, 0.5);
+        prepareNextAction(1000);
+        
+        // Path 4: Move to (44.79, 60.03)
+        moveToPosition(44.79, 60.03, -180, 0.5);
+        prepareNextAction(1000);
+        
+        // Path 5: Move to (19.90, 59.40)
+        moveToPosition(19.90, 59.40, -180, 0.5);
+        prepareNextAction(1000);
+        
+        // Path 6: Move to (68, 83)
+        moveToPosition(68, 83, -180, 0.5);
+        prepareNextAction(1000);
+        
+        // Path 7: Move to (43.37, 35.11)
+        moveToPosition(43.37, 35.11, -180, 0.5);
+        prepareNextAction(1000);
+        
+        // Path 8: Move to (18.40, 35.30)
+        moveToPosition(18.40, 35.30, -180, 0.5);
+        prepareNextAction(1000);
+        
+        // Path 9: Move to (68, 83)
+        moveToPosition(68, 83, -180, 0.5);
+        prepareNextAction(1000);
+
+        // end auto
+
     }
+
+    // if you want to add any functions unique to this auto, do it here
+
 }
