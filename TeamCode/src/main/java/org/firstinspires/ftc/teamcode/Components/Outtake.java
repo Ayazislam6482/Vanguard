26fb517c02407266731b276c32996ff43ddb4f38
MohammadSyed18 <79668964+MohammadSyed18@users.noreply.github.com>
2026-01-09 21:40:45 -0600

    outtake updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Outtake.java
@@ -6,74 +6,93 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+/**
+ * Outtake class for controlling:
+ * - 1 shooter motor (ON/OFF with D-Pad)
+ * - 1 shooter servo (4 preset angles)
+ * - 1 continuous rotation servo (Lazy Susan L2/R2)
+ */
 public class Outtake {
 
     // ---------- Hardware ----------
-    public DcMotorEx turretMotor;
-    public Servo shooterServo;
-    public DcMotorEx shooterMotor;
+    private final DcMotorEx shooterMotor;  // spins shooter
+    public final Servo shooterServo;       // angle adjustment
+    public final Servo lazySusanServo;     // continuous rotation
 
-    // ---------- Shooter angle presets ----------
-    private final double ANGLE_1 = 0.2; // start
-    private final double ANGLE_2 = 0.4;
-    private final double ANGLE_3 = 0.6;
-    private final double ANGLE_4 = 0.8;
+    // ---------- Shooter servo angle presets ----------
+    private final double ANGLE_1 = 0.2; // X button
+    private final double ANGLE_2 = 0.4; // Y button
+    private final double ANGLE_3 = 0.6; // A button
+    private final double ANGLE_4 = 0.8; // B button
 
-    // ---------- Turret rotation speed ----------
-    private final double TURRET_SPEED = 0.6;
+    // ---------- Lazy Susan speed ----------
+    private final double LAZY_SPEED = 0.5; // range 0.0-1.0, 0.5 = stop, >0.5 = forward, <0.5 = reverse
 
     // ---------- Shooter motor power ----------
-    private final double SHOOTER_POWER = 1.0;
+    private final double MOTOR_POWER = 1.0;
 
+    /**
+     * Constructor: initialize hardware
+     */
     public Outtake(HardwareMap hardwareMap) {
-        turretMotor = hardwareMap.get(DcMotorEx.class, "turretMotor");
-        shooterServo = hardwareMap.get(Servo.class, "shooterServo");
         shooterMotor = hardwareMap.get(DcMotorEx.class, "shooterMotor");
+        shooterServo = hardwareMap.get(Servo.class, "shooterServo");
+        lazySusanServo = hardwareMap.get(Servo.class, "lazySusanServo");
     }
 
+    /**
+     * Initialize hardware
+     */
     public void initialize() {
-        // Stop everything initially
-        turretMotor.setPower(0);
+        // Shooter motor
         shooterMotor.setPower(0);
-        shooterServo.setPosition(ANGLE_1); // default angle
-
-        // Directions
-        turretMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         shooterMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        // Brake when power is zero
-        turretMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         shooterMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        // Shooter servo
+        shooterServo.setPosition(ANGLE_1);
+
+        // Lazy Susan stop
+        lazySusanServo.setPower(0); // continuous servo stop
     }
 
-    // Call this in teleop loop
+    /**
+     * Call this every loop in TeleOp
+     */
     public void update(Gamepad gamepad) {
 
-        // ---------- Turret rotation ----------
+        // ---------- Shooter motor ON/OFF ----------
+        if (gamepad.dpad_up) shooterMotor.setPower(MOTOR_POWER);
+        else if (gamepad.dpad_down) shooterMotor.setPower(0);
+
+        // ---------- Shooter servo angles ----------
+        if (gamepad.x) shooterServo.setPosition(ANGLE_1);
+        else if (gamepad.y) shooterServo.setPosition(ANGLE_2);
+        else if (gamepad.a) shooterServo.setPosition(ANGLE_3);
+        else if (gamepad.b) shooterServo.setPosition(ANGLE_4);
+
+        // ---------- Lazy Susan continuous rotation ----------
         if (gamepad.left_trigger > 0.1) {
-            turretMotor.setPower(-TURRET_SPEED); // rotate left
+            lazySusanServo.setPower(-LAZY_SPEED); // rotate left
         } else if (gamepad.right_trigger > 0.1) {
-            turretMotor.setPower(TURRET_SPEED);  // rotate right
+            lazySusanServo.setPower(LAZY_SPEED);  // rotate right
         } else {
-            turretMotor.setPower(0);              // stop
+            lazySusanServo.setPower(0);           // stop
         }
+    }
 
-        // ---------- Shooter angle ----------
-        if (gamepad.y) {
-            shooterServo.setPosition(ANGLE_1);
-        } else if (gamepad.b) {
-            shooterServo.setPosition(ANGLE_2);
-        } else if (gamepad.a) {
-            shooterServo.setPosition(ANGLE_3);
-        } else if (gamepad.x) {
-            shooterServo.setPosition(ANGLE_4);
-        }
+    /**
+     * Optional getters
+     */
+    public double getMotorPower() {
+        return shooterMotor.getPower();
+    }
 
-        // ---------- Shooter motor ----------
-        if (gamepad.right_bumper) {
-            shooterMotor.setPower(SHOOTER_POWER); // spin
-        } else if (gamepad.left_bumper) {
-            shooterMotor.setPower(0);             // stop
-        }
+    public double getShooterServoPosition() {
+        return shooterServo.getPosition();
+    }
+
+    public double getLazySusanPower() {
+        return lazySusanServo.getPower();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TestTeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TestTeleOp1.java
@@ -9,6 +9,7 @@ import org.firstinspires.ftc.teamcode.Components.DriveTrain;
 import org.firstinspires.ftc.teamcode.Components.Intake;
 import org.firstinspires.ftc.teamcode.Components.Pusher;
 import org.firstinspires.ftc.teamcode.Components.ToucherXD;
+import org.firstinspires.ftc.teamcode.Components.Outtake;
 
 @TeleOp(name = "TestTeleOp1")
 public class TestTeleOp1 extends LinearOpMode {
@@ -17,44 +18,32 @@ public class TestTeleOp1 extends LinearOpMode {
     private Intake intake;
     private Pusher pusher;
     private ToucherXD toucherXD;
+    private Outtake outtake;
 
-    // Turret Hardware
+    // Turret Hardware (keep existing for legacy)
     private DcMotorEx turretMotor;
-    private Servo shooterServo;
-    private DcMotorEx shooterMotor;
-
-    // Shooter Preset Angles
-    private final double ANGLE_1 = 0.2;
-    private final double ANGLE_2 = 0.4;
-    private final double ANGLE_3 = 0.6;
-    private final double ANGLE_4 = 0.8;
-
-    private double currentAngle;
 
     @Override
     public void runOpMode() {
 
+        // ----------------------------
         // Initialize Components
+        // ----------------------------
         drivetrain = new DriveTrain(hardwareMap);
         intake = new Intake(hardwareMap);
         pusher = new Pusher(hardwareMap);
         toucherXD = new ToucherXD(hardwareMap);
+        outtake = new Outtake(hardwareMap); // NEW
 
         drivetrain.initialize();
         intake.initialize();
         pusher.initialize();
         toucherXD.initialize();
+        outtake.initialize(); // initialize Outtake
 
-        // Turret Hardware
+        // Turret hardware
         turretMotor = hardwareMap.get(DcMotorEx.class, "turretMotor");
-        shooterServo = hardwareMap.get(Servo.class, "shooterServo");
-        shooterMotor = hardwareMap.get(DcMotorEx.class, "shooterMotor");
-
         turretMotor.setPower(0);
-        shooterMotor.setPower(0);
-
-        currentAngle = ANGLE_1;
-        shooterServo.setPosition(currentAngle);
 
         telemetry.addLine("READY...");
         telemetry.update();
@@ -91,21 +80,6 @@ public class TestTeleOp1 extends LinearOpMode {
                 turretMotor.setPower(0);
 
             // ----------------------------
-            // SHOOTER SERVO ANGLES
-            // ----------------------------
-            if (gamepad2.y)      currentAngle = ANGLE_1;
-            else if (gamepad2.b) currentAngle = ANGLE_2;
-            else if (gamepad2.a) currentAngle = ANGLE_3;
-            else if (gamepad2.x) currentAngle = ANGLE_4;
-
-            shooterServo.setPosition(currentAngle);
-
-            // ----------------------------
-            // SHOOTER MOTOR ON/OFF
-            // ----------------------------
-            shooterMotor.setPower(gamepad2.right_bumper ? 1.0 : 0.0);
-
-            // ----------------------------
             // PUSHER
             // ----------------------------
             if (gamepad2.right_stick_y > 0.1) pusher.pushUp();
@@ -117,11 +91,17 @@ public class TestTeleOp1 extends LinearOpMode {
             toucherXD.update();
 
             // ----------------------------
+            // OUTTAKE UPDATE (shooter + Lazy Susan)
+            // ----------------------------
+            outtake.update(gamepad2);
+
+            // ----------------------------
             // TELEMETRY
             // ----------------------------
             telemetry.addData("Turret Power", turretMotor.getPower());
-            telemetry.addData("Shooter Servo Angle", currentAngle);
-            telemetry.addData("Shooter Motor", shooterMotor.getPower());
+            telemetry.addData("Shooter Motor Power", outtake.getMotorPower());
+            telemetry.addData("Shooter Servo Angle", outtake.getShooterServoPosition());
+            telemetry.addData("Lazy Susan Power", outtake.getLazySusanPower());
             telemetry.addData("Disk State", toucherXD.getCurrentState());
             telemetry.update();
         }
